e9262dba0823268a570986e893f42eb19378a9ac
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-04-08 09:47:31 +0200

    Added controller for accounts and RegisterUserDto class

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccinationCentres.Models.dto;
+
+namespace VaccinationCentres.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+
+        }
+    }
+}
