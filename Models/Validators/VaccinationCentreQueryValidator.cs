48a59568e7d32ec68f4c65bceef476f465cde3cd
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-04-19 09:11:48 +0200

    Added validation rules for pagination of results

---
+++ Models/Validators/VaccinationCentreQueryValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationCentres.Models.Validators
+{
+    public class VaccinationCentreQueryValidator : AbstractValidator<VaccinationCentreQuery>
+    {
+        private int[] allowedPageSizes = new[] {5, 10, 15};
+
+        public VaccinationCentreQueryValidator()
+        {
+            RuleFor(x => x.PageNumber).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.PageSize).Custom((value, context) =>
+            {
+                if(!allowedPageSizes.Contains(value))
+                {
+                    context.AddFailure("PageSize", $"PageSize must in [{string.Join(",", allowedPageSizes)}");
+                }
+            });
+                
+
+        }
+    }
+}
